3fc63435086c123c55e6a9efc997e20c937e8b04
Paulo <pauloh16.pdr@gmail.com>
2024-06-11 22:13:32 -0300

    chore: ajustar alis do vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@types/*': './src/types',
-			'Components/*': './src/components',
-			'Core/*': './src/core',
-			'Hooks/*': './src/hooks',
-			'Assets/*': './src/core/assets',
-			'Constants/*': './src/core/constants',
-			'Helpers/*': './src/core/helpers',
-			'Services/*': './src/core/services',
-			'Contexts/*': './src/contexts',
-			'Pages/*': './src/pages',
-		},
-	},
+	plugins: [react(), tsconfigPaths()],
 });
